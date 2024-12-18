88acd9481bc0b74c64d25801d4a50839ae6968b3
salah <akilsalaheddine203@gmail.com>
2024-12-18 14:51:40 +0100

    first commit

---
+++ src/main/java/org/aura/plateforme_iot/PlateformeIoTApplication.java
@@ -0,0 +1,13 @@
+package org.aura.plateforme_iot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlateformeIoTApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlateformeIoTApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/aura/plateforme_iot/PlateformeIoTApplicationTests.java
@@ -0,0 +1,13 @@
+package org.aura.plateforme_iot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlateformeIoTApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
