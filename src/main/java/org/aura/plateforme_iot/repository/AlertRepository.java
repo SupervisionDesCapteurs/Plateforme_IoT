fdc053b4473aae8dfc23d71f3fec5161ba660583
ATalemsi <mohamadtalemsi@gmail.com>
2024-12-19 13:59:34 +0100

    PIDSDC-18 des modifications

--- src/main/java/org/aura/plateforme_iot/repository/AlertRepository.java
+++ src/main/java/org/aura/plateforme_iot/repository/AlertRepository.java
@@ -1,4 +1,9 @@
 package org.aura.plateforme_iot.repository;
 
-public interface AlertRepository {
+import org.aura.plateforme_iot.entity.Alert;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AlertRepository extends MongoRepository<Alert, String> {
 }
--- src/main/java/org/aura/plateforme_iot/repository/MeasurementRepository.java
+++ src/main/java/org/aura/plateforme_iot/repository/MeasurementRepository.java
@@ -1,4 +1,9 @@
 package org.aura.plateforme_iot.repository;
 
-public interface MeasurementRepository {
+import org.aura.plateforme_iot.entity.Measure;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MeasurementRepository extends MongoRepository<Measure,String> {
 }
