7278948d38465e6adde586dedac8c6c629876bbc
salah <akilsalaheddine203@gmail.com>
2024-12-19 09:21:36 +0100

    add enum package

--- src/main/java/org/aura/plateforme_iot/entity/Device.java
+++ src/main/java/org/aura/plateforme_iot/entity/Device.java
@@ -1,4 +1,14 @@
 package org.aura.plateforme_iot.entity;
 
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document("device")
+@Data
 public class Device {
+    @Id
+    private String id;
+    private String name;
+
 }
---
+++ src/main/java/org/aura/plateforme_iot/entity/Enums/DeviceType.java
@@ -0,0 +1,5 @@
+package org.aura.plateforme_iot.entity.Enums;
+
+public enum DeviceType {
+    Temperature,Humidity
+}
