92f4194a13e6a9db8737882a54d3c4fa02ef0f0a
salah <akilsalaheddine203@gmail.com>
2024-12-19 11:24:24 +0100

    measure mapper

--- src/main/java/org/aura/plateforme_iot/Mapper/MeasureMapper.java
+++ src/main/java/org/aura/plateforme_iot/Mapper/MeasureMapper.java
@@ -1,4 +1,12 @@
 package org.aura.plateforme_iot.Mapper;
 
+import org.aura.plateforme_iot.dto.MeasurementDto;
+import org.aura.plateforme_iot.entity.Measure;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
 public interface MeasureMapper {
+
+    MeasurementDto entityToDTO(Measure measure);
+    Measure measureDTOToEntity(MeasurementDto measureDto);
 }
