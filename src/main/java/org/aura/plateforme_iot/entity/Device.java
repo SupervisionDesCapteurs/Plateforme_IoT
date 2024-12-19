2e664a9d3cdc2cb4d86b6f898dd411aa34495144
salah <akilsalaheddine203@gmail.com>
2024-12-19 09:46:22 +0100

    device model

--- src/main/java/org/aura/plateforme_iot/entity/Device.java
+++ src/main/java/org/aura/plateforme_iot/entity/Device.java
@@ -1,14 +1,24 @@
 package org.aura.plateforme_iot.entity;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
+import org.aura.plateforme_iot.entity.Enums.DeviceStatus;
+import org.aura.plateforme_iot.entity.Enums.DeviceType;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
 
 @Document("device")
 @Data
 public class Device {
     @Id
     private String id;
+    @NotBlank
     private String name;
+    private DeviceType deviceType;
+    private DeviceStatus deviceStatus;
+    private LocalDateTime lastCommunication;
+
 
 }
