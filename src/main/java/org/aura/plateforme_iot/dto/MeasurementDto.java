a4b75b564d32ef6f7402ea585f4654c995cc0744
salah <akilsalaheddine203@gmail.com>
2024-12-19 11:09:27 +0100

    add mesure dto and repository

--- src/main/java/org/aura/plateforme_iot/dto/MeasurementDto.java
+++ src/main/java/org/aura/plateforme_iot/dto/MeasurementDto.java
@@ -1,4 +1,13 @@
 package org.aura.plateforme_iot.dto;
 
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+@Data
+@Builder
 public class MeasurementDto {
+    private LocalDateTime timestamp;
+    private Double value;
+    private String deviceId;
 }
--- src/main/java/org/aura/plateforme_iot/repository/MeasurementRepository.java
+++ src/main/java/org/aura/plateforme_iot/repository/MeasurementRepository.java
@@ -1,4 +1,7 @@
 package org.aura.plateforme_iot.repository;
 
-public interface MeasurementRepository {
+import org.aura.plateforme_iot.entity.Measure;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface MeasurementRepository extends MongoRepository<Measure,String> {
 }
